1969be8c1f44fcdbc9ee441e70ea6ef0284a5ceb
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2019-10-10 15:21:32 +0100

    Add moving points

---
+++ BetaArcade/Assets/KOTH/Scripts/PointMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointMove : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> points = new List<Transform>();
+    Transform previousPos;
+    int pointID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousPos = transform;
+        InvokeRepeating("MovePoint", 1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void MovePoint()
+    {
+        for(int i =0; i<points.Count; ++i)
+        {
+            previousPos.position = transform.position;
+            pointID = Random.Range(0, points.Count);
+            transform.position = points[pointID].transform.position;
+            if (previousPos.position == transform.position)
+            {
+                pointID = Random.Range(0, points.Count);
+                transform.position = points[pointID].transform.position;
+            }
+        }
+    
+    }
+}
---
+++ BetaArcade/Assets/KOTH/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    TextMeshProUGUI scoreText;
+    int score;
+    PointMove point;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        point = GameObject.Find("Point").GetComponent<PointMove>();
+        scoreText = GameObject.Find("Counter").GetComponent<TextMeshProUGUI>();
+        scoreText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Point")
+        {
+            InvokeRepeating("AddScore", 0, 1);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Point")
+        {
+            CancelInvoke("AddScore");
+        }
+    }
+    void AddScore()
+    {
+        score += 1;
+        scoreText.text = "" + score;
+        Debug.Log("Add point");
+
+    }
+}
