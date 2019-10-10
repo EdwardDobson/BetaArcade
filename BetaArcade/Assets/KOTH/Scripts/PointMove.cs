c6e9cbc06f59cf8127aba0b5aef25db7d8d098c8
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2019-10-10 16:13:45 +0100

    Update

--- BetaArcade/Assets/KOTH/Scripts/PointMove.cs
+++ BetaArcade/Assets/KOTH/Scripts/PointMove.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PointMove : MonoBehaviour
 {
     [SerializeField]
     List<Transform> points = new List<Transform>();
     Transform previousPos;
+    TextMeshProUGUI moveText;
     int pointID;
     // Start is called before the first frame update
     void Start()
     {
         previousPos = transform;
         InvokeRepeating("MovePoint", 1, 5);
+        moveText = GameObject.Find("MoveText").GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
@@ -32,7 +35,14 @@ public class PointMove : MonoBehaviour
                 pointID = Random.Range(0, points.Count);
                 transform.position = points[pointID].transform.position;
             }
+            moveText.text = "Point has moved!";
+            StartCoroutine(HideText());
         }
     
+    }
+    IEnumerator HideText()
+    {
+        yield return new WaitForSeconds(1);
+        moveText.text = "";
     }
 }
