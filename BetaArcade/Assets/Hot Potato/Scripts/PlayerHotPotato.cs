a3f2fccbf653f0d784df4c0c945f808e3758f19d
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2019-10-11 13:27:39 +0100

    Update
    Currently the bomb switches players every x amount of seconds and when every player is dead it resets the players

---
+++ BetaArcade/Assets/Hot Potato/Scripts/PlayerHotPotato.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHotPotato : MonoBehaviour
+{
+    [SerializeField]
+    bool hasBomb;
+    GameObject bombImage;
+    void Start()
+    {
+        bombImage = transform.GetChild(0).GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasBomb)
+        {
+            bombImage.SetActive(true);
+        }
+        else bombImage.SetActive(false);
+    }
+    public void SetHasBomb(bool _bool)
+    {
+        hasBomb = _bool;
+    }
+    public bool HasBomb()
+    {
+        return hasBomb;
+    }
+}
