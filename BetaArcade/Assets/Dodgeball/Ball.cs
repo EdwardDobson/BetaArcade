a66570b2e6db05b210421600150efbaa6b105791
ConnorJamesDawson <47003934+ConnorJamesDawson@users.noreply.github.com>
2019-10-20 20:00:16 +0100

    Added Code for Dodgeball
    Need to build scene

---
+++ BetaArcade/Assets/Dodgeball/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public bool IsActive = false;
+
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+    public GameObject Player4;
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player1" || col.gameObject.tag == "Player2" || col.gameObject.tag == "Player3" || col.gameObject.tag == "Player4")
+        {
+            if (IsActive) // if the player doesn't have anything
+                col.gameObject.SetActive(false);
+            Debug.Log("Player Down");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ BetaArcade/Assets/Dodgeball/PickUp_Throw.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp_Throw : MonoBehaviour
+{
+    public float speed = 10;
+    public bool canHold = true;
+    public GameObject Ball;
+    public Transform guide;
+    private bool ivepressedabutton = false;
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1") && !ivepressedabutton)
+        {
+            ivepressedabutton = true;
+            if (!canHold)
+                ThrowOrDrop();
+            else
+            {
+                Pickup();
+            }
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            ivepressedabutton = false;
+        }
+        //Ball.transform.position = guide.position; //Moves the ball if you can't hold the ball
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Ball")
+        {
+            if (!Ball) // if the player doesn't have anything
+                Ball = col.gameObject;
+            Debug.Log("Touched Ball");
+        }
+    }
+
+    private void Pickup()
+    {
+        if (!Ball) //If we don' have a ball
+            return;
+        //We set the object parent to our guide empty object i.e become it's child
+        Ball.transform.SetParent(guide);
+        //Set gravity to false while holding it
+        Ball.GetComponent<Rigidbody>().useGravity = false;
+        //we apply the same rotation our main object (Camera) has.
+        Ball.transform.localRotation = transform.rotation;
+        //We re-position the ball on our guide object
+        Ball.transform.position = guide.position;
+        //Set the ball to be active
+        Ball.GetComponent<Ball>().IsActive = true;
+        canHold = false;
+    }
+    private void ThrowOrDrop()
+    {
+        if (!Ball)
+            return;
+        //Set our Gravity to true again.
+        Ball.GetComponent<Rigidbody>().useGravity = true;
+        // we don't have anything to do with our ball anymore
+        Ball = null;
+        //Apply velocity on throwing
+        guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+        //Unparent the ball
+        guide.GetChild(0).parent = null;
+        canHold = true;
+    }
+
+}
