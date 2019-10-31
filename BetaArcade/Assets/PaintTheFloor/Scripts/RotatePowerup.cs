cb5d2b70243e401ef54f4b1922732e5515255d49
Kamil <theoriginalpole@gmail.com>
2019-10-31 12:11:55 +0000

    Improved powerups

--- BetaArcade/Assets/PaintTheFloor/Scripts/RotatePowerup.cs
+++ BetaArcade/Assets/PaintTheFloor/Scripts/RotatePowerup.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotatePowerup : MonoBehaviour
-  {
-  System.Random rand;
-  // Start is called before the first frame update
-  void Start()
+{
+    System.Random rand;
+    int x;
+    int y;
+    int z;
+    // Start is called before the first frame update
+    void Start()
     {
-    int seed = 0;
-    rand = new System.Random(seed);
+        rand = new System.Random(GetInstanceID());
+        x = rand.Next(0, 360);
+        y = rand.Next(0, 360);
+        z = rand.Next(0, 360);
     }
 
-  // Update is called once per frame
-  void Update()
+    // Update is called once per frame
+    void Update()
     {
-    transform.RotateAround(GetComponent<Renderer>().bounds.center, new Vector3(rand.Next(0, 360), rand.Next(0, 360), rand.Next(0, 360)), 90 * Time.deltaTime);
+        //transform.RotateAround(GetComponent<Renderer>().bounds.center, new Vector3(rand.Next(0, 360), rand.Next(0, 360), rand.Next(0, 360)), 90 * Time.deltaTime);
+        transform.Rotate(new Vector3(x,y, z), Time.deltaTime * 100);
     }
-  }
+}
