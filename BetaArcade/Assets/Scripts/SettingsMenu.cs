0250020b542a78bc8e5029ab66dfaf47306e2966
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2019-11-08 18:54:19 +0000

    Made options menu

---
+++ BetaArcade/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Toggle fullscreenToggle;
+    public Slider volume;
+    public TMP_Dropdown qualitySettings;
+    bool isFullscreen;
+    Resolution[] resolutions;
+    public void Start()
+    {
+        isFullscreen = PlayerPrefs.GetInt("Fullscreen") == 1 ? true : false;
+        Screen.fullScreen = isFullscreen;
+        fullscreenToggle.isOn = isFullscreen;
+        Screen.SetResolution(1920, 1080, Screen.fullScreen);
+        audioMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("Volume"));
+        volume.value = PlayerPrefs.GetFloat("Volume");
+        QualitySettings.SetQualityLevel(PlayerPrefs.GetInt("QualitySettings"));
+        qualitySettings.value = PlayerPrefs.GetInt("QualitySettings");
+    }
+    public void SetVolume(float _value)
+    {
+        audioMixer.SetFloat("MasterVolume", _value);
+        PlayerPrefs.SetFloat("Volume", _value);
+    }
+    public void SetQuality(int _index)
+    {
+        QualitySettings.SetQualityLevel(_index);
+        PlayerPrefs.SetInt("QualitySettings", _index);
+    }
+    public void SetFullscreen(bool _isFullscreen)
+    {
+        Screen.fullScreen = _isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
+
+    }
+}
