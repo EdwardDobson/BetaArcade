50b9341dcf89c85e0356abf3f6a8b9545c6a948f
Kamil <theoriginalpole@gmail.com>
2019-11-09 12:04:22 +0000

    Added timer to tron, fixed tron collisions

--- BetaArcade/Assets/TronTest/Scripts/LevelManager.cs
+++ BetaArcade/Assets/TronTest/Scripts/LevelManager.cs
@@ -7,15 +7,27 @@ public class LevelManager : MonoBehaviour
   {
   public GameObject PlayerObject;
 
-  private int playerCount = 0;
+  private int m_PlayerCount = 0;
+  private float m_Timer;
+  private float m_MaxTime = 90f;
+  private GameObject[] players;
+  public int TargetPlayers
+    {
+    set
+      {
+      for(int i = m_PlayerCount; i < value; i++)
+        {
+        CreatePlayer();
+        }
+      }
+    }
 
   // Start is called before the first frame update
   void Start()
     {
-    CreatePlayer();
-    CreatePlayer();
-    CreatePlayer();
-    CreatePlayer();
+    m_Timer = m_MaxTime;
+    TargetPlayers = 4;
+    players = GameObject.FindObjectsOfType(typeof(GameObject)).Where(x => (x as GameObject).tag.Contains("Player")).Select(x => x as GameObject).ToArray();
     }
 
   private void Update()
@@ -27,19 +39,30 @@ public class LevelManager : MonoBehaviour
         CreatePlayer();
         }
       }
-    GameObject[] players = GameObject.FindObjectsOfType(typeof(GameObject)).Where(x => (x as GameObject).tag.Contains("Player")).Select(x => x as GameObject).ToArray();
-    if (players.Length == 1)
+
+    if(players.Where(x => x != null).Count() == 1)
       {
-      players.ElementAt(0).GetComponent<PlayerManager>().SetWinner();
+      players.First(x => x != null).GetComponent<PlayerManager>().SetWinner();
       }
+
+    m_Timer -= Time.deltaTime;
+    if(m_Timer <= 0.0f)
+      {
+      End();
+      }
+    }
+
+  private void End()
+    {
+    // Loop through players and check who won
     }
 
   void CreatePlayer()
     {
     var player = Instantiate(PlayerObject);
     var playerScript = player.GetComponent<PlayerManager>();
-    player.transform.position = new Vector3((10 * playerCount++) - 15, 1);
-    playerScript.ID = playerCount;
-    player.GetComponent<Renderer>().material.SetColor("_BaseColor", LevelManagerTools.PlayerIDToColor(playerCount));
+    player.transform.position = new Vector3((10 * m_PlayerCount++) - 15, 1);
+    playerScript.ID = m_PlayerCount;
+    player.GetComponent<Renderer>().material.SetColor("_BaseColor", LevelManagerTools.PlayerIDToColor(m_PlayerCount));
     }
   }
