4f39b0b14c9fe67f6facd644fa28ab22bda16450
CStaves <47003831+CStaves@users.noreply.github.com>
2019-11-21 12:53:47 +0000

    Updated the HP Stage
    Added Textures
    Added in a change floor script, needs testing.

---
+++ BetaArcade/Assets/Hot Potato/DiscoFloor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoFloor : MonoBehaviour
+{
+    public Texture[] textures;
+    public float changeInterval;
+    public Renderer rend;
+    public float Timer;
+    int textureIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (textures.Length == 0)
+            return;
+        Timer -= Time.deltaTime;
+        if(Timer <=0)
+        {
+            Timer = changeInterval;
+
+            textureIndex++;
+
+            rend.material.SetTexture(textures[1].name, textures[1]);
+            if(textureIndex >= textures.Length)
+            {
+                textureIndex = 0;
+            }
+        }
+      
+    }
+}
