954e293600af2f8eff35811ec84620843e661699
natxtw <36550708+natxtw@users.noreply.github.com>
2019-11-21 12:45:22 +0000

    Players Die now
    title.

--- BetaArcade/Assets/PaintBall-Tag/Scripts/Killed.cs
+++ BetaArcade/Assets/PaintBall-Tag/Scripts/Killed.cs
@@ -1,25 +1,37 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Killed : MonoBehaviour
 {
-    public float Health = 150;
-    public Shooting ShootingScript;
+    public float Health = 50.0f;
+    private Shooting ShootingScript;
 
     void Start()
-    { 
-       
+    {
+       ShootingScript = GetComponent<Shooting>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    void OnCollisionEnter(Collider Bullet)
+    void OnCollisionEnter(Collision col)
     {
-        
+        ShootingScript = GetComponentsInChildren<Transform>().Where(x => x.gameObject.activeSelf).First(x => x.GetComponent<Shooting>() != null).GetComponent<Shooting>();
+
+        if (col.gameObject.tag == "Bullet")
+        {
+            Health -= ShootingScript.Damage;
+            Debug.Log("i did damage");
+            Destroy(col.gameObject);
+        }
     }
+
 }
