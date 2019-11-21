a07381bb9e93d3a78d9b476071290795cfecdcd8
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2019-11-21 14:40:52 +0000

    Update
    Added icon image slots for pickups
    Changed the scale of the icon image slots on the UI

--- BetaArcade/Assets/PaintTheFloor/Scripts/Powerups/IncreaseShotSize.cs
+++ BetaArcade/Assets/PaintTheFloor/Scripts/Powerups/IncreaseShotSize.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class IncreaseShotSize : MonoBehaviour
-  {
-  private void OnTriggerEnter(Collider other)
+{
+    public Sprite Icon;
+    private void OnTriggerEnter(Collider other)
     {
-    if (other.tag.Contains("Player"))
-      {
-      GameObject Clone = GameObject.Find("PlayerPicture" + other.gameObject.GetComponent<PlayerMove>().ID);
-      other.gameObject.GetComponent<PTFMovement>().ShotSize = 5;
-      other.gameObject.GetComponent<PlayerMove>().IncreasePowerUpCount(1);
-
-      if (Clone != null)
+        if (other.tag.Contains("Player"))
         {
-        Clone.transform.GetChild(other.gameObject.GetComponent<PlayerMove>().GetPowerUpCount()).GetComponent<Image>().color = new Vector4(1, 1, 1, 1);
-        Clone.transform.GetChild(other.gameObject.GetComponent<PlayerMove>().GetPowerUpCount()).name = name;
-        }
+            GameObject Clone = GameObject.Find("PlayerPicture" + other.gameObject.GetComponent<PlayerMove>().ID);
+            other.gameObject.GetComponent<PTFMovement>().ShotSize = 5;
+            other.gameObject.GetComponent<PlayerMove>().IncreasePowerUpCount(1);
 
-      Destroy(gameObject);
-      }
+            if (Clone != null)
+            {
+                Clone.transform.GetChild(other.gameObject.GetComponent<PlayerMove>().GetPowerUpCount()).GetComponent<Image>().sprite = Icon;
+                Clone.transform.GetChild(other.gameObject.GetComponent<PlayerMove>().GetPowerUpCount()).GetComponent<Image>().color = new Vector4(1, 1, 1, 1);
+                Clone.transform.GetChild(other.gameObject.GetComponent<PlayerMove>().GetPowerUpCount()).name = name;
+            }
+
+            Destroy(gameObject);
+        }
     }
-  }
+}
