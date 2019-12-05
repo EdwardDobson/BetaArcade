bf9b7ea5b12a1cdf3154681fa8f65c2b098e6ada
CStaves <47003831+CStaves@users.noreply.github.com>
2019-12-05 12:55:36 +0000

    Stage done

--- BetaArcade/Assets/Hot Potato/DiscoFloor.cs
+++ BetaArcade/Assets/Hot Potato/DiscoFloor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DiscoFloor : MonoBehaviour
 {
-    public Texture[] textures;
+    public Material[] materials;
     public float changeInterval;
     public Renderer rend;
     public float Timer;
@@ -17,49 +17,16 @@ public class DiscoFloor : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    { 
-        
+    {
         Timer -= Time.deltaTime;
-
+ 
         if(Timer <=0)
         {
             Timer = changeInterval;
             textureIndex++;
-            textureIndex %= textures.Length;
-            rend.material.mainTexture = textures[textureIndex];
+            textureIndex %= materials.Length;
+            rend.material = materials[textureIndex];
         }
         
-
-        /*
-         if (textures.Length == 0)
-             return;
-         Timer -= Time.deltaTime;
-         if(Timer <=0)
-         {
-             Timer = changeInterval;
-
-             textureIndex++;
-
-             rend.material.SetTexture("_BaseMap", textures[textureIndex]);
-             if(textureIndex >= textures.Length)
-             {
-                 textureIndex = 0;
-             }
-             Debug.Log("Texture changed");
-
-         }
-
-            if (textures.Length == 0)
-            return;
-
-            textureIndex = Mathf.FloorToInt(Time.time / changeInterval);
-            textureIndex = textureIndex % textures.Length;
-            rend.material.mainTexture = textures[textureIndex];
-
-
-
-         */
-        
-       
     }
 }
