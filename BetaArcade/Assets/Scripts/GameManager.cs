124dd59a37f404afdaab4e9811fa63715661de3c
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2020-01-01 20:11:23 +0000

    Merged branches and reduced UI creation code

--- BetaArcade/Assets/Scripts/GameManager.cs
+++ BetaArcade/Assets/Scripts/GameManager.cs
@@ -217,62 +217,42 @@ public class GameManager : MonoBehaviour
     public void CreatePlayerUI()
     {
         GameObject playerUI = Instantiate(PlayerPicture);
-
-        if (playerCount == 0)
-        {
-            playerUI.GetComponent<Image>().color = Color.red;
-            playerUI.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(1, 0, 0, 0.3f);
-            playerUI.transform.GetChild(7).GetChild(0).GetComponent<Image>().color = new Color(1, 0, 0, 0.3f);
-            playerUI.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.red;
-            playerUI.transform.GetChild(7).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.red;
-            playerUI.transform.GetComponent<Image>().color = Color.red;
-            playerUI.transform.GetChild(8).GetComponent<Image>().color = Color.red;
-            playerUI.transform.GetChild(9).GetComponent<Image>().color = Color.red;
-        }
-        if (playerCount == 1)
-        {
-            playerUI.GetComponent<Image>().color = Color.yellow;
-            playerUI.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(1, 1, 0, 0.3f);
-            playerUI.transform.GetChild(7).GetChild(0).GetComponent<Image>().color = new Color(0, 0, 1, 0.3f);
-            playerUI.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.yellow;
-            playerUI.transform.GetChild(7).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.yellow;
-            playerUI.transform.GetChild(8).GetComponent<Image>().color = Color.yellow;
-            playerUI.transform.GetChild(9).GetComponent<Image>().color = Color.yellow;
-        }
-        if (playerCount == 2)
-        {
-            playerUI.GetComponent<Image>().color = Color.green;
-            playerUI.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(0, 1, 0, 0.3f);
-            playerUI.transform.GetChild(7).GetChild(0).GetComponent<Image>().color = new Color(0, 1, 0, 0.3f);
-            playerUI.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.green;
-            playerUI.transform.GetChild(7).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.green;
-            playerUI.transform.GetChild(8).GetComponent<Image>().color = Color.green;
-            playerUI.transform.GetChild(9).GetComponent<Image>().color = Color.green;
-        }
-        if (playerCount == 3)
-        {
-            playerUI.GetComponent<Image>().color = Color.blue;
-            playerUI.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = new Color(0, 0, 1, 0.3f);
-            playerUI.transform.GetChild(7).GetChild(0).GetComponent<Image>().color = new Color(0, 0, 1, 0.3f);
-            playerUI.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.blue;
-            playerUI.transform.GetChild(7).GetChild(1).GetChild(0).GetComponent<Image>().color = Color.blue;
-            playerUI.transform.GetChild(8).GetComponent<Image>().color = Color.blue;
-            playerUI.transform.GetChild(9).GetComponent<Image>().color = Color.blue;
-        }
-
         playerUI.transform.position = Portraits[playerCount].position;
         playerCount++;
         playerUI.name = "PlayerPicture" + playerCount;
-        playerUI.transform.GetChild(1).GetComponent<Image>().color = new Color(1, 1, 1, 0);
-        playerUI.transform.GetChild(2).GetComponent<Image>().color = new Color(1, 1, 1, 0);
-        playerUI.transform.GetChild(3).GetComponent<Image>().color = new Color(1, 1, 1, 0);
-        playerUI.transform.GetChild(4).GetComponent<Image>().color = new Color(1, 1, 1, 0);
-        playerUI.transform.GetChild(5).GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        for(int i = 1; i < 6; ++i)
+        {
+            playerUI.transform.GetChild(i).GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        }
+        playerUI.GetComponent<Image>().color = PlayerUIImageColour(playerCount);
+        playerUI.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = PlayerUIImageColour(playerCount);
+        playerUI.transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>().color = PlayerUIImageColour(playerCount);
+        playerUI.transform.GetChild(7).GetChild(0).GetComponent<Image>().color = PlayerUIImageColour(playerCount);
+        playerUI.transform.GetChild(7).GetChild(1).GetChild(0).GetComponent<Image>().color = PlayerUIImageColour(playerCount);
+        playerUI.transform.GetChild(8).GetComponent<Image>().color = PlayerUIImageColour(playerCount);
+        playerUI.transform.GetChild(9).GetComponent<Image>().color = PlayerUIImageColour(playerCount);
         playerUI.transform.GetChild(6).GetComponent<TextMeshProUGUI>().text = "";
-        
         playerUI.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
         playerUI.transform.SetParent(GameObject.Find("PlayerUI").transform.GetChild(1).transform);
         PlayerPictures.Add(playerUI);
+    }
+    private Color PlayerUIImageColour(int id)
+    {
+        switch (id)
+        {
+            case 1:
+                return Color.red;
+            case 2:
+                return Color.yellow;
+            case 3:
+                return Color.green;
+            case 4:
+                return Color.blue;
+            default:
+                Debug.LogError("Player has no ID");
+                break;
+        }
+        return Color.clear;
     }
     public void AddToPlayListName(string _levelName)
     {
